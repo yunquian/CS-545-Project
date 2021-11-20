819a9ff79b9b0249f022da146a8f23144d8ff546
MichaelFan36 <51762741+MichaelFan36@users.noreply.github.com>
2021-11-20 01:01:06 -0800

    VAE added

--- models/vae.py
+++ models/vae.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
+import torch
 
 from env import n_fft
 
@@ -8,10 +9,26 @@ class ToyVAE(nn.Module):
     def __init__(self, frame_size=n_fft // 2 + 1):
         super().__init__()
         self.fc1 = nn.Linear(frame_size, 512)
-        self.fc2 = nn.Linear(512, 512)
+        self.mu = nn.Linear(512, 10)
+        self.log_var = nn.Linear(512, 10)
+        self.fc2 = nn.Linear(10, 512)
         self.fc3 = nn.Linear(512, frame_size)
+        self.sigmoid = nn.Sigmoid()
+
+    def encode(self, x):
+        h1 = F.relu(self.fc1(x))
+        return self.mu(h1), self.log_var(h1)
+
+    def decode(self, z):
+        h2 = F.relu(self.fc2(z))
+        return self.sigmoid(self.fc3(h2))
+
+    def calculate(self, mu, log_var):
+        std = torch.exp(0.5*log_var)
+        eps = torch.randn_like(std)
+        return mu + eps * std
 
     def forward(self, x):
-        return self.fc3(
-            F.leaky_relu(self.fc2(
-                F.leaky_relu(self.fc1(x), negative_slope=0.2))))
+        mu, log_var = self.encode(x.view(-1, n_fft // 2 + 1))
+        z = self.calculate(mu, log_var)
+        return self.decode(z), mu, log_var
